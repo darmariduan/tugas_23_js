86e7f1dd69a4f0cb83afaf952852a6893a053e9d
Darma Riduan <dharmarhiduwhan@gmail.com>
2020-01-21 08:44:41 +0700

    Tugas 23 Js

---
+++ tugas23.js
@@ -0,0 +1,8 @@
+function tugas23() {
+  var data = [ 32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66 ];
+  var limabelas = data.filter(data =>{
+    return data > 15;
+  })
+  console.log(data);
+}
+tugas23()
